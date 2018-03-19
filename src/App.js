817c5edc957f61912deda5b7828fa4f612d8b9ab
Laptop <laptop@Laptops-MacBook.home>
2018-03-19 12:46:59 -0400

    update to GA

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ import Home from './components/Home'
 // import About from './components/About'
 // import ViewPortfolioPiece from './components/ViewPortfolioPiece'
 
-ReactGA.initialize('UA-112854595-1')
-ReactGA.pageview(window.location.pathname)
+ReactGA.initialize('UA-112854595-1', {debug: true})
+ReactGA.set({ page: window.location.pathname })
+ReactGA.pageview( window.location.pathname)
 
 const history = createHistory()
 history.listen((location, action) => {
-  ReactGA.set({ page: location.pathname })
-  ReactGA.pageview(location.pathname)
-  console.log(location.pathname)
+  ReactGA.set({ page: location.hash })
+  ReactGA.pageview(location.pathname + location.hash)
+  console.log(location.pathname + location.hash)
 })
 
 
