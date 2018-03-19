eb10ec64ca78a9bf57c888f3a29dffd746c8517a
Laptop <laptop@Laptops-MacBook.home>
2018-03-19 12:26:50 -0400

    updated to GA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import {
-  BrowserRouter as Router,
+  Router,
   Route
 } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
 import ReactGA from 'react-ga'
+import createHistory from 'history/createBrowserHistory'
+
 
 import Home from './components/Home'
 // import About from './components/About'
@@ -12,13 +14,21 @@ import Home from './components/Home'
 
 ReactGA.initialize('UA-112854595-1')
 
+const history = createHistory()
+history.listen((location, action) => {
+  ReactGA.set({ page: location.pathname })
+  ReactGA.pageview(location.pathname)
+  console.log(location.pathname)
+})
+
+
 export default class App extends Component {
 
   render() {
 
     return (
 
-      <Router onUpdate={ logPageView }>
+      <Router history={ history }>
         <ScrollToTop>
           <main>
             <Route exact path="/" component={ Home }/>
@@ -32,10 +42,4 @@ export default class App extends Component {
 
   }
 
-}
-
-function logPageView() {
-    var path = /\/.*(?=\?)/.exec(window.location.hash)[0]
-    ReactGA.set({ page: path });
-    ReactGA.pageview(path);
 }
