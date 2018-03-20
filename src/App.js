cc46f7ab2f3d1d18e8d8b89cd8b72e29ae42a160
Laptop <laptop@Laptops-MacBook.home>
2018-03-20 12:47:08 -0400

    removed bad line

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
 
     return (
 
-      <Router history={ history }>
+      <Router>
         <ScrollToTop>
           <main>
             <Route exact path="/" component={ Home }/>
